06aa0f7047c34b801e42aee5c0561ae40d7c90dc
rushil-shah87 <54685822+rushil-shah87@users.noreply.github.com>
2020-11-07 12:55:36 -0500

    final updates

--- a09/twitter.js
+++ a09/twitter.js
@@ -12,6 +12,7 @@ async function renderFeed() {
     let likes;
     let retweets;
     let id;
+    let num_replies;
     for (let i = 0; i < 50; i++) {
         type = tweets.data[i].type;
         body = tweets.data[i].body;
@@ -21,10 +22,12 @@ async function renderFeed() {
         likes = tweets.data[i].likeCount;
         retweets = tweets.data[i].retweetCount;
         id = tweets.data[i].id;
+        num_replies = tweets.data[i].replyCount;
 
         let tweet = document.createElement("div");
         tweet.classList.add("card", "column", "is-one-third", "is-spaced")
         tweet.setAttribute("tweet-id", id);
+        tweet.setAttribute("type", type);
         let content_div = document.createElement("div");
         content_div.classList.add("card-content");
         let tweetinfo_div = document.createElement("div");
@@ -52,17 +55,34 @@ async function renderFeed() {
         if (tweets.data[i].isLiked) {
             likes_p.innerHTML += " (you)";
         }
+        let replies_p = document.createElement("p");
+        replies_p.classList.add("is-small")
+        replies_p.innerHTML = num_replies.toString() + ' replies';
         let lr_btns = document.createElement("div");
         lr_btns.classList.add("column");
         let like_btn = document.createElement("button");
         like_btn.classList.add("button", "is-dark", "is-outlined", "like-tweet-btn", "is-small");
-        like_btn.innerHTML = "Toggle Like";
+        like_btn.innerHTML = "Like";
         let rtwt_btn = document.createElement("button");
         rtwt_btn.classList.add("button", "is-dark", "is-outlined", "rtwt-btn", "is-small");
         rtwt_btn.innerHTML = "Retweet";
         let reply_btn = document.createElement("button");
         reply_btn.classList.add("button", "is-dark", "is-outlined", "reply-btn", "is-small");
         reply_btn.innerHTML = "Reply";
+        let footer_div = document.createElement("footer");
+        footer_div.classList.add("card-footer", "no-border", "is-centered", "og-ftr");
+        let second_footer = document.createElement("footer");
+        second_footer.classList.add("card-footer", "no-border", "is-centered", "second-ftr");
+
+        if (tweets.data[i].isMine) {
+            tweet.setAttribute("mine", 'true');
+            let edit_button = renderEditButton();
+            let delete_button = renderDeleteButton();
+            footer_div.appendChild(edit_button);
+            footer_div.appendChild(delete_button);
+            $('.edit-button').on('click', handleEditTweet);
+            $('.delete-button').on('click', handleDeleteTweet);
+        }
 
         tweetinfo_div.appendChild(author_p);
         tweetinfo_div.appendChild(time_p);
@@ -71,6 +91,7 @@ async function renderFeed() {
         content_div.appendChild(body_div);
         lr_info.appendChild(likes_p);
         lr_info.appendChild(retweets_p);
+        lr_info.appendChild(replies_p);
         likesretweets.appendChild(lr_info);
         lr_btns.appendChild(like_btn);
         lr_btns.appendChild(rtwt_btn);
@@ -78,23 +99,14 @@ async function renderFeed() {
         likesretweets.appendChild(lr_btns);
         content_div.appendChild(likesretweets);
         tweet.appendChild(content_div);
-
-        if (tweets.data[i].isMine) {
-            let footer_div = document.createElement("footer");
-            footer_div.classList.add("card-footer", "no-border", "is-centered");
-            let edit_button = renderEditButton();
-            $('.edit-button').on('click', handleEditTweet);
-            let delete_button = renderDeleteButton();
-            $('.delete-button').on('click', handleDeleteTweet);
-            footer_div.appendChild(edit_button);
-            footer_div.appendChild(delete_button);
-            tweet.appendChild(footer_div);
-        }
+        tweet.appendChild(footer_div);
+        tweet.appendChild(second_footer);
 
         document.getElementById("root").appendChild(tweet);
     }
     $('.like-tweet-btn').on('click', handleLikeButton);
-    $('.rtwt-btn').on('click', handleRetweet);
+    $('.rtwt-btn').on('click', handleRetweetBtn);
+    $('.reply-btn').on('click', handleReplyBtn);
 };
 
 function renderEditButton() {
@@ -143,7 +155,7 @@ function renderTweetEditor(body) {
 
 function renderSaveEditButton(tweet_id) {
     return `
-    <button class="button save-edits-button" tweet-id="${tweet_id}">Save</button>
+    <button class="button save-edits-button">Update</button>
     `;
 }
 
@@ -155,10 +167,9 @@ function renderCancelEditButton() {
 
 function handleEditTweet(event) {
     let tweet = $(event.target).closest('.card').find('.content').html();
-    let tweet_id = $(event.target).closest('.card').attr('tweet-id');
     $(event.target).closest('.card').find('.content').replaceWith(renderTweetEditor(tweet));
     $(event.target).closest('.card').find('.delete-button').replaceWith(renderCancelEditButton());
-    $(event.target).closest('.card').find('.edit-button').replaceWith(renderSaveEditButton(tweet_id));
+    $(event.target).closest('.card').find('.edit-button').replaceWith(renderSaveEditButton());
     $('.save-edits-button').on('click', handleSaveEdits);
     $('.cancel-edits-button').on('click', handleCancelEdits);
 }
@@ -167,6 +178,7 @@ async function handleSaveEdits(event) {
     let body = $(event.target).closest('.card').find('#written-tweet').val().toString();
     let id = $(event.target).closest('.card').attr("tweet-id");
     let url = base_url + id;
+    $(event.target).closest('.card').find('.og-ftr').children().addClass('is-success');
     const result = await axios({
         method: 'put',
         url,
@@ -175,14 +187,17 @@ async function handleSaveEdits(event) {
             body,
         },
     });
+    handleCancelEdits(event);
+    location.reload();
 }
 
 // for cancel button when editing a tweet
 function handleCancelEdits(event) {
     let tweet_id = $(event.target).closest('.card').attr('tweet-id');
-    $(event.target).closest('.card').find('#written-tweet').replaceWith(renderTweetByID(tweet_id));
-    $(event.target).closest('.card').find('.save-edits-button').replaceWith(renderEditButton());
     $(event.target).closest('.card').find('.cancel-edits-button').replaceWith(renderDeleteButton());
+    $(event.target).closest('.card').find('.save-edits-button').replaceWith(renderEditButton());
+    $(event.target).closest('.card').find('#written-tweet').replaceWith(renderTweetByID(tweet_id));
+    $(event.target).closest('.card').find('.og-ftr').children().removeClass('is-success');
     $('.edit-button').on('click', handleEditTweet);
     $('.delete-button').on('click', handleDeleteTweet);
 }
@@ -221,6 +236,7 @@ async function handleSendTweet(event) {
         }
     });
     handleCancelTweet();
+    location.reload();
 }
 
 async function handleLikeButton(event) {
@@ -231,19 +247,66 @@ async function handleLikeButton(event) {
         url,
         withCredentials: true,
     });
+    location.reload();
 }
 
 function renderRetweetReplyBox() {
     return `
     <textarea id="rt-reply" class="textarea is-info" placeholder="Write a retweet/reply" 
-    rows="2"></textarea> <br>
+    rows="2"></textarea>
     `;
 }
 
-async function handleRetweet(event) {
-    $(renderRetweetReplyBox()).insertBefore($(event.target).closest('.card').find($('.card-footer')));
+function renderSendRtReplyBtn() {
+    return `<button class="button send-r-btn">Send</button>`;
+}
+
+function renderCancelRtReplyBtn() {
+    return `<button class="button cancel-r-btn">Cancel</button>`;
+
+}
+
+function handleRetweetBtn(event) {
+    $(renderRetweetReplyBox()).insertBefore($(event.target).closest('.card').find($('.og-ftr')));
+    $(event.target).closest('.card').find('.og-ftr').children().addClass('is-loading');
+    $(event.target).closest('.card').find('.second-ftr').append(renderSendRtReplyBtn());
+    $(event.target).closest('.card').find('.second-ftr').append(renderCancelRtReplyBtn());
+    $('.send-r-btn').on('click', handleSendRt);
+    $('.cancel-r-btn').on('click', handleCancelRtReply);
+}
+
+function handleReplyBtn(event) {
+    $(renderRetweetReplyBox()).insertBefore($(event.target).closest('.card').find($('.og-ftr')));
+    $(event.target).closest('.card').find('.og-ftr').children().addClass('is-loading');
+    $(event.target).closest('.card').find('.second-ftr').append(renderSendRtReplyBtn());
+    $(event.target).closest('.card').find('.second-ftr').append(renderCancelRtReplyBtn());
+    $('.send-r-btn').on('click', handleSendReply);
+    $('.cancel-r-btn').on('click', handleCancelRtReply);
+}
+
+async function handleSendReply(event) {
     let tweet_id = $(event.target).closest('.card').attr("tweet-id");
-    let body = $('#rt-reply').html();
+    let body = $(event.target).closest('.card').find('#rt-reply').val().toString();
+    $(event.target).closest('.card').find('.second-ftr').children().addClass('is-success');
+    const result = await axios({
+        method: 'post',
+        url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets',
+        withCredentials: true,
+        data: {
+            "type": "reply",
+            "parent": tweet_id,
+            body,
+        },
+    });
+    handleCancelRtReply(event);
+    location.reload();
+}
+
+async function handleSendRt(event) {
+    let tweet_id = $(event.target).closest('.card').attr("tweet-id");
+    let body = $(event.target).closest('.card').find('#rt-reply').val().toString();
+    body += "<br> [Rt - " + $(event.target).closest('.card').find('.content').html() + "]";
+    $(event.target).closest('.card').find('.second-ftr').children().addClass('is-success');
     const result = await axios({
         method: 'post',
         url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets',
@@ -254,6 +317,14 @@ async function handleRetweet(event) {
             body,
         },
     });
+    handleCancelRtReply(event);
+    location.reload();
+}
+
+function handleCancelRtReply(event) {
+    $(event.target).closest('.card').find('.og-ftr').children().removeClass('is-loading');
+    $(event.target).closest('.card').find('#rt-reply').remove();
+    $(event.target).closest('.card').find('.second-ftr').children().remove();
 }
 
 function setListeners() {
